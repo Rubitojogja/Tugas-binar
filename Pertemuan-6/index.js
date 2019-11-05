a43c0f24ea4cdc48d79678388e27d6a0ae5d297f
dilo-rubito <rubitojogja@gmail.com>
2019-11-05 15:21:29 +0700

    pert 6

---
+++ Pertemuan-6/index.js
@@ -0,0 +1,101 @@
+
+//Example 1 | Declaration
+// function pertambahan (num1, num2){
+//     console.log(num1+num2);
+// }
+// pertambahan(5, 5);
+
+//Example 2 | Expression
+// let pengurangan = function(num1, num2){
+//     console.log(num1 - num2);
+// }
+// pengurangan(10, 4);
+
+
+//Example 3 | Arrow function
+// let perkalian =(num1, num2) =>{
+//     console.log(num1 * num2);
+// }
+// perkalian (2, 6);
+
+//example 4 | Exponensial / pangkat
+// let Exponensial = function(num1, num2){
+//     console.log(num1 ** num2);
+// }
+// Exponensial(2, 5);
+
+//LOOPING
+//for loop
+// for (let i = 0; i< 5; i++){
+//     console.log(`Nilainya adalah : ${i}`);
+// }
+
+// for ...in
+//menggunakan backtik untuk menyimpan string ke par (index(``))
+
+// let names =['Andi','Tono','Tini','Budi']
+// for (x in names){
+//     if (names [x] == 'Tono'){   
+//     console.log(`index: ${x}, name: ${names[3]}`);
+// }
+// }
+
+
+//for ...Of
+
+// let fruits =['Banana','Mango','Apple','Orange'];
+// for (i of fruits){
+//     console.log(`My name is ${i}`);
+// }
+
+// while
+// let num = 1;
+// while (num < 10){
+//     console.log('Hello World');
+//     num++;
+// }
+
+//.forEach
+
+// let ages = [11,23,45,12,34,32];
+// ages.forEach((item, index, array) =>{
+//     console.log(`item: ${item}, index: ${index}, array: ${array}`);
+// });
+
+//.map
+// let numbers =[1,2,3,1,2,3,5,7];
+// let newNumber = numbers.map(function(num){
+//     return num *3;
+// });
+// console.log(newNumber);
+
+// atau 
+
+// let numbers =[1,2,3,1,2,3,5,7];
+// let newNumber = numbers.map((num) => {
+//     return num *3;
+// });
+// console.log(newNumber);
+
+
+// let nama = Bambang;
+// let hobi = hobi saya memancing; 
+// let umur = 29;
+// function myProfile(nama, umur, hobi);
+// if (umur < 56){
+//     console.log(nama, )
+// } 
+let nama =Bambang;
+let umur = 29;
+if (umur < 56) {
+  console.log("saya masih aktif bekerja ");
+} else {
+  console.log("angka ini lebih kecil atau sama dengan 5");
+}
+
+
+
+// function calculator (operator, angka1, angka2){
+
+// }
+// calculator('pembagian', 1,5);
